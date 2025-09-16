3837c0abda2a6e9f49976b83dd5fc90071802522
AmiyaPiyush1 <gamingzone.09990@gmail.com>
2025-09-17 00:49:54 +0530

    hosted

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(cors({
     origin: [
+        'https://resumebuilderbackend.web.app',
         'https://resumebuilderbackend.web.app', // Replace with your deployed frontend URL
         'http://localhost:3000'
     ]
